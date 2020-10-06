97a48cff5fa99d61105c2131079d0bda22a3f71f
naman <naman.agarwal2019@vitstudent.ac.in>
2020-10-06 15:09:30 +0530

    audio fix test

--- views/script.js
+++ views/script.js
@@ -23,9 +23,36 @@
 //   .then(gotLocalMediaStream)
 //   .catch(handleLocalMediaStreamError);
 
+
+
+// cope with browser differences
+let audioContext;
+if (typeof AudioContext === 'function') {
+  audioContext = new AudioContext();
+} else if (typeof webkitAudioContext === 'function') {
+  audioContext = new webkitAudioContext(); // eslint-disable-line new-cap
+} else {
+  console.log('Sorry! Web Audio not supported.');
+}
+
+// create a filter node
+var filterNode = audioContext.createBiquadFilter();
+// see https://dvcs.w3.org/hg/audio/raw-file/tip/webaudio/specification.html#BiquadFilterNode-section
+filterNode.type = 'highpass';
+// cutoff frequency: for highpass, audio is attenuated below this frequency
+filterNode.frequency.value = 10000;
+
+// create a gain node (to change audio volume)
+var gainNode = audioContext.createGain();
+// default is 1 (no change); less than 1 means audio is attenuated
+// and vice versa
+gainNode.gain.value = 0.5;
+
+
+
 var url = location.origin.replace(/^http/, 'ws')
 const signaling = new WebSocket(url);
-const constraints = {  video: true};
+const constraints = {  video: true, audio: true };
 var dataConstraint = null;
 // var configuration = { 
 //   "iceServers": [{ "url": "stun:stun.stunprotocol.org" }] 
@@ -55,7 +82,13 @@ async function start() {
   try {
     // get local stream, show it in self-view and add it to be sent
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
-    pc.addStream(stream);
+    const mediaStreamSource =
+    audioContext.createMediaStreamSource(stream);
+    mediaStreamSource.connect(filterNode);
+   filterNode.connect(gainNode);
+  // connect the gain node to the destination (i.e. play the sound)
+   gainNode.connect(audioContext.destination);
+    pc.addStream(stream); 
     selfView = document.getElementById("local-video");
     selfView.srcObject = stream;
   } catch (err) {
