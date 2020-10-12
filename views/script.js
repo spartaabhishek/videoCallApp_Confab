8da487a67c200cebfdabc6228f67607ef5770696
naman <naman.agarwal2019@vitstudent.ac.in>
2020-10-13 01:23:42 +0530

    fixes

--- views/script.js
+++ views/script.js
@@ -39,13 +39,15 @@ var url = location.origin.replace(/^http/, 'ws')
 // signalling - a medium to share info between two peers (clients)
 const signaling = new WebSocket(url);
 
-// incoming video constraints
+//incoming video constraints
 const constraints = {video: {
   width: { min: 640, max: 1024 },
   height: { min: 480, max: 768 },
   aspectRatio: { ideal: 1.7777777778 }
 },
 audio: true};
+
+
 
 
 
@@ -66,7 +68,12 @@ const config = {
           url: 'turn:numb.viagenie.ca',
           credential: 'muazkh',
           username: 'webrtc@live.com'
-      }
+      },
+      {url:'stun:stun.l.google.com:19302'},
+      {url:'stun:stun2.l.google.com:19302'},
+      {url:'stun:stun3.l.google.com:19302'},
+      {url:'stun:stun4.l.google.com:19302'},
+      {url:'stun:stunserver.org'}
   ]
 }
 
@@ -99,6 +106,7 @@ async function start(pc) {
           pc.addTrack(track, stream);
       })
   }
+      
       let selfView = document.getElementById("local-video");
       console.log("set local video")
       selfView.srcObject = stream;
@@ -208,20 +216,36 @@ async function handleOffer(i){
       // rtcpeerconnection -> remote desc and local desc
       // offer -> initiating connection peers
       // offer contains info about ip , video constraints and encryption 
-      var offer = await peers[i]["con"].createOffer()
-      if(peers[i]["con"].localDescription==null){
+      
+      if(peers[i]["con"].localDescription==null && peers[i]['con'].remoteDescription==null){
+        var offer = await peers[i]["con"].createOffer()
         await peers[i]["con"].setLocalDescription(new RTCSessionDescription (offer))
         console.log(`set local for ${i}`)
+      
+        // send the offer to the other peer
+        await send({ offer: new RTCSessionDescription(offer),type:'offer',roomId, userA : i ,userB:userId});
+      }else{
+        console.log('already connected')
       }
-
-      // send the offer to the other peer
-      await send({ offer: new RTCSessionDescription(offer),type:'offer',roomId, userA : i ,userB:userId});
     }
     catch (err) {
       console.error(err);
     }
   }
 
+
+  async function connectOtherUsers(){
+
+    await send({type:"users",roomId})
+    var userList = roomUsers.reverse()
+    let i = roomUsers.indexOf(userId)+1
+    for(;i<roomUsers.length;i++){
+      if(roomUsers[i]!=userId){
+        console.log("connectOthers"+i)
+        handleOffer(roomUsers[i])
+    }
+  }
+}
 
 // incoming messages from server (websocket)
 signaling.addEventListener('message',async (msg) => {
@@ -255,35 +279,41 @@ signaling.addEventListener('message',async (msg) => {
       // Respoing to an incoming offer with an answer
       else if(data.type === "offer") {
 
+        if(peers[data.userB]["con"].remoteDescription==null && peers[data.userB]["con"].localDescription==null){
         await peers[data.userB]["con"].setRemoteDescription(new RTCSessionDescription(data.offer));
         //const stream = await navigator.mediaDevices.getUserMedia(constraints);
         var answer = await peers[data.userB]["con"].createAnswer()
         if(peers[data.userB]["con"].localDescription==null){
           await peers[data.userB]["con"].setLocalDescription(new RTCSessionDescription(answer))
         }
-        console.log(data)
+        // console.log(data)
         await send({ answer: new RTCSessionDescription(answer),type:'answer',roomId,userA:data.userB,userB:userId});
-        console.log("offer signal")
-        console.log(data.offer)
-        //sendChannel.send(selfView.value);
+        
+        // console.log("offer signal")
+        // console.log(data.offer)
+      }
+      
       } 
       
       
       // Handling Incoming answer to Our Offer
       else if (data.type === "answer") {
-        console.log("answer")
-        
+        // console.log("answer")
+        if(peers[data.userB]["con"].remoteDescription==null){
         await peers[data.userB]["con"].setRemoteDescription(await new RTCSessionDescription(data.answer));
-        console.log("answer signal")
-        console.log(data.answer)
         
+        // console.log("answer signal")
+        // console.log(data.answer)
+        }
       } 
 
     // STUN share Info
      else if (data.type==="candidate") {
+       if(peers[data.userId]["con"].remoteDescription!=null){
         peers[data.userId]["con"].addIceCandidate(new RTCIceCandidate(data.candidate));
-        console.log("candidate signal")
-        console.log(data.candidate)
+        // console.log("candidate signal")
+        // console.log(data.candidate)
+       }
         
       }
 
